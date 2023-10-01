309428e106b04842f4f2b2905b496119fae6a94e
Brian French <fr3nch13@gmail.com>
2023-10-01 13:28:46 -0700

    Initial login layout.

---
+++ templates/layout/login.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The default layout
+ *
+ * @var \App\View\AppView $this
+ */
+
+$this->extend('base');
+
+$this->start('layout');
+
+?>
+<body>
+
+<?= $this->Template->templateComment(true, __FILE__); ?>
+<?= $this->element('nav/top'); ?>
+
+<?= $this->Flash->render() ?>
+<?= $this->fetch('content') ?>
+
+<?= $this->element('nav/footer'); ?>
+
+<?= $this->Html->script(['libs.bundle', 'index.bundle']) ?></body>
+<?= $this->Template->templateComment(false, __FILE__); ?>
+<?php $this->end() // layout ?>
