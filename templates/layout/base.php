b0a137a7f89191ed549174f0f57f1025ef52b665
Brian French <fr3nch13@gmail.com>
2023-10-01 13:26:54 -0700

    favicon and layout changes.

--- templates/layout/base.php
+++ templates/layout/base.php
@@ -12,15 +12,12 @@
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+    <?= $this->Html->meta('icon') ?>
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <link rel="manifest" href="/img/site.webmanifest">
-    <title>
-        <?= $this->fetch('title') ?>
-    </title>
-    <?= $this->Html->meta('icon') ?>
+    <title><?= $this->fetch('title') ?></title>
 
     <?= $this->Html->css([
         'libs.bundle',
@@ -34,10 +31,8 @@
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
-<body class="bg-light">
 
 <?= $this->fetch('layout'); ?>
 
-<?= $this->Html->script(['libs.bundle', 'index.bundle']) ?></body>
 <?= $this->Template->templateComment(false, __FILE__); ?>
 </html>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -10,27 +10,16 @@ $this->extend('base');
 $this->start('layout');
 
 ?>
+<body class="bg-light">
+
 <?= $this->Template->templateComment(true, __FILE__); ?>
 <?= $this->element('nav/top'); ?>
+
 <?= $this->Flash->render() ?>
 <?= $this->fetch('content') ?>
+
 <?= $this->element('nav/footer'); ?>
-<!--
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-        </div>
-    </main>
-    <footer>
-    </footer>
--->
+
+<?= $this->Html->script(['libs.bundle', 'index.bundle']) ?></body>
 <?= $this->Template->templateComment(false, __FILE__); ?>
 <?php $this->end() // layout ?>
