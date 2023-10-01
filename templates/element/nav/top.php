b8f631fe24cab362a6f916d849cff9cc7274d2f7
Brian French <fr3nch13@gmail.com>
2023-10-01 16:54:55 -0700

    Update top.php

--- templates/element/nav/top.php
+++ templates/element/nav/top.php
@@ -3,9 +3,14 @@
  * @var \App\View\AppView $this
  *
  */
+
+$navClasses = 'navbar navbar-expand-lg navbar-sticky navbar-light bg-light border-bottom';
+if ($this->getLayout() === 'login') {
+    $navClasses = 'navbar navbar-expand-lg navbar-sticky navbar-dark';
+}
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
-<nav id="mainNav" class="navbar navbar-expand-lg navbar-sticky navbar-light bg-light border-bottom">
+<nav id="mainNav" class="<?= $navClasses; ?>">
     <div class="container">
       <?= $this->Html->link($this->Html->image('logo_dark.png', ['class' => 'logo-top']), '/', ['escape' => false]); ?>
       <?= $this->element('nav/top_right'); ?>
