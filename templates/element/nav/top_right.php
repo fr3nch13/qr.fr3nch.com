6286cbddc7eda0b017bfd425f24101a1a41255cc
Brian French <fr3nch13@gmail.com>
2023-10-02 14:07:29 -0700

    Mobile navbar fixed?

--- templates/element/nav/top_right.php
+++ templates/element/nav/top_right.php
@@ -6,44 +6,44 @@
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
       <!-- secondary -->
-      <ul class="navbar-nav navbar-nav-secondary order-lg-3">
-        <?php if ($this->ActiveUser->getUser()): ?>
-        <li class="nav-item d-lg-none">
-          <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#userNav" aria-expanded="false">
+<ul class="navbar-nav navbar-nav-secondary order-lg-3">
+    <?php if ($this->ActiveUser->getUser()): ?>
+    <li class="nav-item d-lg-none">
+        <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#userNav" aria-expanded="false">
             <i class="bi bi-person"></i>
-          </a>
-        </li>
-        <!-- The user icon and dropdown for user-specific pages -->
-        <li class="nav-item dropdown dropdown-hover d-none d-lg-block">
-          <a class="nav-link nav-icon" role="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        </a>
+    </li>
+    <!-- The user icon and dropdown for user-specific pages -->
+    <li class="nav-item dropdown dropdown-hover d-none d-lg-block">
+        <a class="nav-link nav-icon" role="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="bi bi-person"></i>
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
             <li><a class="dropdown-item active" href="./account.html">Dashboard</a></li>
             <li><a class="dropdown-item " href="./account-settings.html">Settings</a></li>
             <li><a class="dropdown-item " href="./account-orders.html">Orders</a></li>
             <li><a class="dropdown-item " href="./account-billing.html">Billing</a></li>
             <li><a class="dropdown-item text-red" href="#">Log Out</a></li>
-          </ul>
-        </li>
-        <li class="nav-item d-lg-none">
-          <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        </ul>
+    </li>
+    <?php else: ?>
+    <!-- Signup Link -->
+    <li class="nav-item d-none d-lg-block">
+        <?php
+        $classes = 'btn btn-primary rounded-pill ms-2';
+        if ($this->getLayout() === 'login') {
+            $classes = 'btn btn-outline-white rounded-pill ms-2';
+        }
+        echo $this->Html->link(__('Sign In'), [
+        'controller' => 'Users',
+        'action' => 'login'
+        ], ['class' => $classes]); ?>
+    </li>
+    <?php endif; ?>
+    <li class="nav-item d-lg-none">
+        <a class="nav-link nav-icon" href="" role="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="bi bi-list"></span>
-          </a>
-        </li>
-        <?php else: ?>
-        <!-- Signup Link -->
-        <li class="nav-item d-none d-lg-block">
-          <?php
-            $classes = 'btn btn-primary rounded-pill ms-2';
-            if ($this->getLayout() === 'login') {
-                $classes = 'btn btn-outline-white rounded-pill ms-2';
-            }
-            echo $this->Html->link(__('Sign In'), [
-            'controller' => 'Users',
-            'action' => 'login'
-          ], ['class' => $classes]); ?>
-        </li>
-        <?php endif; ?>
-      </ul>
+        </a>
+    </li>
+</ul>
 <?= $this->Template->templateComment(false, __FILE__); ?>
