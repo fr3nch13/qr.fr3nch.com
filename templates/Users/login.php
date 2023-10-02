71139d7fdc35e14832977143d52c714b2a6f21f2
Brian French <fr3nch13@gmail.com>
2023-10-02 13:47:22 -0700

    Update login.php

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -56,51 +56,14 @@ $this->start('card_body');
                     <?php
                     // TODO: Add logic for forgetting passwords.
                     ?>
-                    <?= $this->Html->link('Forgot Password?', [
+                    <?php /* echo $this->Html->link('Forgot Password?', [
                         'controller' => 'Users',
                         'action' => 'forgot_password',
-                    ]); ?>
+                    ]);*/ ?>
                   </div>
                 </div>
                 <?= $this->Form->end() ?>
               </div>
-
-              <!--
-                <div class="d-grid mb-2">
-                  <a href="" class="btn btn-lg btn-primary">Sign In</a>
-                </div>
-                <div class="submit"><input type="submit" class="btn-primary btn" value="Login"></div>
-
-
-              <form action="#">
-
-                <div class="form-floating mb-2">
-                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                  <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating mb-2">
-                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                  <label for="floatingPassword">Password</label>
-                </div>
-                <div class="d-grid mb-2">
-                  <a href="" class="btn btn-lg btn-primary">Sign In</a>
-                </div>
-                <div class="row">
-                  <div class="col">
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                      <label class="form-check-label small text-secondary" for="defaultCheck1">
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col text-end">
-                    <a href="forgot-password.html" class="underline small">Forgot password?</a>
-                  </div>
-                </div>
-              </form>
-              -->
-
 
 <?= $this->Template->templateComment(false, __FILE__); ?>
 <?php
