ebb4230909d6446f5ed1e9be36dae0ab2b6267b2
Brian French <fr3nch13@gmail.com>
2023-10-01 21:05:07 -0700

    Update base.php

--- templates/layout/base.php
+++ templates/layout/base.php
@@ -24,6 +24,7 @@ if ($this->get('body_options')) {
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     <link rel="manifest" href="/img/site.webmanifest">
+    <?= $this->Paginator->meta(); ?>
     <title><?= $this->fetch('title') ?></title>
 
     <?= $this->Html->css([
