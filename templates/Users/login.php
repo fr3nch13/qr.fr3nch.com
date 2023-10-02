8b31cbedb84aef2e1d7c9897f557b55ab80282f3
Brian French <fr3nch13@gmail.com>
2023-10-02 13:49:40 -0700

    Update login.php

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -14,56 +14,51 @@ $this->assign('card_title', __('Sign In'));
 $this->start('card_body');
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
-                <!--
-              <div class="d-grid">
-                <a href="" class="btn btn-outline-red btn-with-icon text-white-hover">Sign In with Google <i
-                    class="bi bi-google"></i></a>
-              </div>
-              <div class="text-muted text-center small py-2">or use your email</div>
-              -->
-              <div class="users form">
-                <?= $this->Form->create() ?>
-                <fieldset>
-                    <legend class="text-muted text-center small py-2"><?= __('Please enter your email and password') ?></legend>
+<div class="users form">
+    <?= $this->Form->create() ?>
+        <fieldset>
+            <legend class="text-muted text-center small py-2"><?= __('Please enter your email and password') ?></legend>
 
-                    <?= $this->Form->control('email', [
-                        'required' => true,
-                        'spacing' => 'mb-2',
-                        'label' => ['floating' => true],
-                    ]) ?>
+            <?= $this->Form->control('email', [
+                'required' => true,
+                'spacing' => 'mb-2',
+                'label' => ['floating' => true],
+            ]) ?>
 
-                    <?= $this->Form->control('password', [
-                        'required' => true,
-                        'spacing' => 'mb-2',
-                        'label' => ['floating' => true],
-                    ]) ?>
+            <?= $this->Form->control('password', [
+                'required' => true,
+                'spacing' => 'mb-2',
+                'label' => ['floating' => true],
+            ]) ?>
 
-                </fieldset>
-                <div class="d-grid mb-2">
-                    <?= $this->Form->button('Sign In', [
-                        'type' => 'submit',
-                        'class' => 'btn btn-lg btn-primary btn-block',
-                    ]); ?>
-                </div>
-                <div class="row">
-                  <div class="col">
-                    <?= $this->Form->control('remember_me', [
-                        'type' => 'checkbox',
-                        'label' => __('Remember Me?'),
-                    ]); ?>
-                  </div>
-                  <div class="col text-end">
-                    <?php
-                    // TODO: Add logic for forgetting passwords.
-                    ?>
-                    <?php /* echo $this->Html->link('Forgot Password?', [
-                        'controller' => 'Users',
-                        'action' => 'forgot_password',
-                    ]);*/ ?>
-                  </div>
-                </div>
-                <?= $this->Form->end() ?>
-              </div>
+        </fieldset>
+        <div class="d-grid mb-2">
+            <?= $this->Form->button('Sign In', [
+                'type' => 'submit',
+                'class' => 'btn btn-lg btn-primary btn-block',
+            ]); ?>
+        </div>
+        <div class="row">
+            <div class="col">
+                <?= $this->Form->control('remember_me', [
+                    'type' => 'checkbox',
+                    'label' => __('Remember Me?'),
+                ]); ?>
+            </div>
+            <!--s
+            <div class="col text-end">
+                <?php
+                // TODO: Add logic for forgetting passwords.
+                ?>
+                <?= $this->Html->link('Forgot Password?', [
+                    'controller' => 'Users',
+                    'action' => 'forgot_password',
+                ]); ?>
+            </div>
+            -->
+        </div>
+    <?= $this->Form->end() ?>
+</div>
 
 <?= $this->Template->templateComment(false, __FILE__); ?>
 <?php
