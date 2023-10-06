bc3d0099a7bb087573e05aa84f754402b8e0ecd1
Brian French <fr3nch13@gmail.com>
2023-10-06 14:53:28 -0700

    Lets see if even a post is working.

--- src/Controller/QrCodesController.php
+++ src/Controller/QrCodesController.php
@@ -208,12 +208,12 @@ class QrCodesController extends AppController
      */
     public function edit(?string $id = null): ?Response
     {
-        $this->request->allowMethod(['get', 'put']);
+        $this->request->allowMethod(['get', 'post']);
 
         $qrCode = $this->QrCodes->get((int)$id, contain: ['Categories', 'Tags']);
         $this->Authorization->authorize($qrCode);
 
-        if ($this->request->is('put')) {
+        if ($this->request->is('post')) {
             $qrCode = $this->QrCodes->patchEntity($qrCode, $this->request->getData());
             if ($this->QrCodes->save($qrCode)) {
                 $this->Flash->success(__('The qr code has been saved.'));
