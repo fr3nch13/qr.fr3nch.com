8762cd0a5fa5dcefd81b3d586fc9e8886cf8d087
Brian French <fr3nch13@gmail.com>
2023-10-07 14:36:00 -0700

    Update index.php

--- templates/QrCodes/index.php
+++ templates/QrCodes/index.php
@@ -197,6 +197,26 @@ if (!$this->getRequest()->is('ajax')) {
                     <div class="row g-0">
                         <div class="col">
                             <?= $this->Form->text('q', [
+                                'class' => 'form-control form-control px-2 text-primary',
+                                'placeholder' => __('What are you looking for ?'),
+                            ]); ?>
+                        </div>
+                        <div class="col-auto d-grid">
+                            <?= $this->Form->button('<i class="bi bi-search"></i>', [
+                                'type' => 'submit',
+                                'class' => 'btn btn-primary btn-icon rounded-circle',
+                                'escapeTitle' => false,
+                            ]); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="widget mb-2">
+                <span class="d-flex eyebrow text-muted mb-2"><?= __('Source') ?></span>
+                <div class="grouped-inputs p-1 rounded-pill border">
+                    <div class="row g-0">
+                        <div class="col">
+                            <?= $this->Form->text('q', [
                                 'class' => 'form-control form-control px-4 text-primary',
                                 'placeholder' => __('What are you looking for ?'),
                             ]); ?>
