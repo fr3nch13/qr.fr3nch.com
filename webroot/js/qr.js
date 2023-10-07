7134e33771a2074f3411ffa517195a57ad2535cb
Brian French <fr3nch13@gmail.com>
2023-10-07 16:16:52 -0700

    Update qr.js

--- webroot/js/qr.js
+++ webroot/js/qr.js
@@ -3,6 +3,6 @@ $( document ).ready(function() {
     // Watch the Source select filter dropdown
 
     $('#filterSource').change(function(){
-        alert($('#filterSource option:selected').text());
+        window.location.href = '/?s=' + $('#filterSource option:selected').text();
     })
 });
