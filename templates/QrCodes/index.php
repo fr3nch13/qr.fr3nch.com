4fdcf19d1f6232d46358044ab09be90510afed06
Brian French <fr3nch13@gmail.com>
2023-10-06 20:03:11 -0700

    Update index.php

--- templates/QrCodes/index.php
+++ templates/QrCodes/index.php
@@ -173,7 +173,12 @@ if (!$this->getRequest()->is('ajax')) {
                 'valueSources' => 'query',
             ]);
             // Match the search param in your table configuration
-            echo $this->Form->control('q');
+            echo $this->Form->control('q', [
+                'label' => [
+                    'value' => __('Search'),
+                    'floating' => true,
+                ],
+            ]);
             echo $this->Form->button('Filter', ['type' => 'submit']);
             echo $this->Html->link('Reset', ['action' => 'index']);
             echo $this->Form->end();
