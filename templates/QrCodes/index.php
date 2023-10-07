5db70a51735c0795833081f30afac98fb7fba664
Brian French <fr3nch13@gmail.com>
2023-10-07 16:11:55 -0700

    Update index.php

--- templates/QrCodes/index.php
+++ templates/QrCodes/index.php
@@ -217,7 +217,7 @@ if (!$this->getRequest()->is('ajax')) {
                         <div class="col">
                             <?= $this->Form->select('s', $sources,[
                                 'empty' => __('Select a Source'),
-                                'onchange' => 'alert($(this).text());',
+                                'id' => 'filterSOurce',
                             ]); ?>
                         </div>
                     </div>
