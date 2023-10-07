9d9a53b13a9e93ecf5a7a0de31e459b6a17c0afd
Brian French <fr3nch13@gmail.com>
2023-10-06 19:56:26 -0700

    default form

--- templates/QrCodes/index.php
+++ templates/QrCodes/index.php
@@ -167,7 +167,14 @@ if (!$this->getRequest()->is('ajax')) {
         </div>
         <div class="offcanvas-body">
             <div class="widget">
-                    Search form here.
+            <?php
+            echo $this->Form->create(null, ['valueSources' => 'query']);
+            // Match the search param in your table configuration
+            echo $this->Form->control('q');
+            echo $this->Form->button('Filter', ['type' => 'submit']);
+            echo $this->Html->link('Reset', ['action' => 'index']);
+            echo $this->Form->end();
+            ?>
             </div>
 
     <!--
