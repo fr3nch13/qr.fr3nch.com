818dd2e66d18f35624e938235b881d4cb4dac124
Brian French <fr3nch13@gmail.com>
2023-10-07 16:25:39 -0700

    Update qr.js

--- webroot/js/qr.js
+++ webroot/js/qr.js
@@ -1,8 +1,13 @@
 $( document ).ready(function() {
 
     // Watch the Source select filter dropdown
-
     $('#filterSource').change(function(){
+        // TODO: instead of redirecting, do an ajax call
+        // labels: forms, jquery, ajax
         window.location.href = '/?s=' + $('#filterSource option:selected').text();
+    })
+    // Watch the Tag select filter dropdown
+    $('#filterTag').change(function(){
+        window.location.href = '/?t=' + $('#filterTag option:selected').text();
     })
 });
