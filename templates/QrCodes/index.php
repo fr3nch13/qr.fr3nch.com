0bc6a4c5beb8e44794b100f1b5a38d017fe15e1b
Brian French <fr3nch13@gmail.com>
2023-10-07 20:29:20 -0700

    Update index.php

--- templates/QrCodes/index.php
+++ templates/QrCodes/index.php
@@ -194,7 +194,7 @@ if (!$this->getRequest()->is('ajax')) {
             <div class="widget mb-2">
                 <?= $this->Form->control('t',[
                     'options' => $tags,
-                    'empty' => ' ',
+                    'empty' => '[select]',
                     'label' => [
                         'floating' => true,
                         'text' => __('Select a Tag'),
@@ -204,7 +204,7 @@ if (!$this->getRequest()->is('ajax')) {
             <div class="widget mb-2">
                 <?= $this->Form->control('s',[
                     'options' => $sources,
-                    'empty' => ' ',
+                    'empty' => '[select]',
                     'label' => [
                         'floating' => true,
                         'text' => __('Select a Source'),
