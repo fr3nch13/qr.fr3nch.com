dd4716b6a431fc17eb1151fd9eab2d179a645f2b
Brian French <fr3nch13@gmail.com>
2023-10-11 23:09:24 -0700

    Update view.php

--- templates/Admin/QrCodes/view.php
+++ templates/Admin/QrCodes/view.php
@@ -9,11 +9,39 @@ if (!$this->getRequest()->is('ajax')) {
 }
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
+<div class="row">
+    <div class="col-lg-6">
+        <dl class="row">
+            <dt class="col-sm-3"><?= __('Key') ?></dt>
+            <dd class="col-sm-9"><?= h($qrCode->qrkey) ?> </dd>
 
-<img
-    class="img-fluid"
-    src="<?= $this->Url->build(['action' => 'show', $qrCode->id]) ?>"
-    alt="<?= __('The QR Code'); ?>">
+            <dt class="col-sm-3"><?= __('URL') ?></dt>
+            <dd class="col-sm-9"><?= $qrCode->url ?> </dd>
+
+            <dt class="col-sm-3"><?= __('Hits') ?></dt>
+            <dd class="col-sm-9"><?= h($qrCode->last_hit) ?> </dd>
+
+            <dt class="col-sm-3"><?= __('Last Hit') ?></dt>
+            <dd class="col-sm-9"><?= h($qrCode->last_hit) ?> </dd>
+
+            <dt class="col-sm-3"><?= __('Created') ?></dt>
+            <dd class="col-sm-9"><?= h($qrCode->created) ?> </dd>
+
+            <dt class="col-sm-3"><?= __('Source') ?></dt>
+            <dd class="col-sm-9"><?= $qrCode->hasValue('source') ? $qrCode->source->name : '' ?> </dd>
+
+            <dt class="col-sm-3"><?= __('Owner') ?></dt>
+            <dd class="col-sm-9"><?= $qrCode->hasValue('user') ? $qrCode->user->name : '' ?> </dd>
+        </dl>
+    </div>
+    <div class="col-lg-6">
+        <img
+            class="img-fluid"
+            src="<?= $this->Url->build(['action' => 'show', $qrCode->id]) ?>"
+            alt="<?= __('The QR Code'); ?>">
+    </div>
+</div>
+
 
 <div class="product-view">
 sdfasdfasdf
