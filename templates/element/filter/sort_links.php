410bce7825ccdd52a8823e16469487ef2cc8be5b
Brian French <fr3nch13@gmail.com>
2023-10-12 14:20:58 -0700

    Update sort_links.php

--- templates/element/filter/sort_links.php
+++ templates/element/filter/sort_links.php
@@ -7,20 +7,29 @@
 if (!isset($class)) {
     $class = 'btn btn-light rounded-pill mx-1 my-1';
 }
+$sort = null;
+$queryParams = $this->getRequest()->getQueryParams();
+if (isset($queryParams['sort'])) {
+    $sort = $queryParams['sort'];
+}
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
 <?php foreach ($sorts as $key => $name) {
     $nameAsc = $name . ' <i class="bi bi-chevron-down"></i>';
     $nameDesc = $name . ' <i class="bi bi-chevron-up"></i>';
 
+    $classActive = null;
     if (str_contains($class, 'btn')) {
+        if ($sort == $key) {
+            $classActive = ' btn-outline-dark';
+        }
         $nameAsc = $this->Html->tag('span', $nameAsc, [
-            'class' => $class,
+            'class' => $class . $classActive,
             'role' => 'button',
         ]);
 
         $nameDesc = $this->Html->tag('span', $nameDesc, [
-            'class' => $class,
+            'class' => $class . $classActive,
             'role' => 'button',
         ]);
     }
