7c1bd10f8163f7c2a441ca20ec2ce8f11ad91a6c
Brian French <fr3nch13@gmail.com>
2023-10-11 22:57:28 -0700

    Update details.php

--- templates/Admin/QrCodes/details.php
+++ templates/Admin/QrCodes/details.php
@@ -15,30 +15,40 @@ $tabs = [
 ];
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
-<h2><?= $qrCode->name ?></h2>
 
 <section>
-    <ul class="nav nav-tabs">
-    <?php foreach ($tabs as $k => $tab) :
-        $options = [
-            'class' => 'nav-link',
-        ];
-        if ($k === $action) {
-            $options['class'] .= ' active';
-            $options['aria-current'] = 'page';
-        }
-        ?>
-        <li class="nav-item">
-            <?= $this->Html->link($tab[0], $tab[1], $options) ?>
-        </li>
-    <?php endforeach; ?>
-    </ul>
+        <div class="row g-3 g-md-5 mb-5">
+            <div class="col-md-6">
+                <h2><?= $qrCode->name ?></h2>
+            </div>
 
-    <div class="card bg-opaque-white">
-        <div class="card-body bg-white">
-            <?= $this->fetch('content'); ?>
+            <div class="col-md-6 text-md-end">
+                <ul class="nav nav-tabs">
+                <?php foreach ($tabs as $k => $tab) :
+                    $options = [
+                        'class' => 'nav-link',
+                    ];
+                    if ($k === $action) {
+                        $options['class'] .= ' active';
+                        $options['aria-current'] = 'page';
+                    }
+                    ?>
+                    <li class="nav-item">
+                        <?= $this->Html->link($tab[0], $tab[1], $options) ?>
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col">
+                <div class="card bg-opaque-white">
+                    <div class="card-body bg-white">
+                        <?= $this->fetch('content'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 </section>
 
