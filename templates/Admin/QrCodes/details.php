5703b2076655e24675c6af2f7b6a3cb25696d0c3
Brian French <fr3nch13@gmail.com>
2023-10-11 22:00:55 -0700

    Update details.php

--- templates/Admin/QrCodes/details.php
+++ templates/Admin/QrCodes/details.php
@@ -6,12 +6,20 @@
 if (!$this->getRequest()->is('ajax')) {
     $this->setLayout('dashboard/view');
 }
+$action = $this->getRequest()->getAttribute('action');
+print_r($action);
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
 <h2><?= $qrCode->name ?></h2>
 <ul class="nav nav-tabs">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
+    <?= $this->Html->link(__('Details'), [
+        'controler' => 'QrCodes',
+        'action' => 'view',
+        $qrCode->id,
+    ], [
+        'class' => 'nav-link',
+    ]) ?>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Link</a>
