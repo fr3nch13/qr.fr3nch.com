37e0ae461468dd83c9633de90df99fa1a52bb30c
Brian French <fr3nch13@gmail.com>
2023-10-12 13:28:14 -0700

    Changing view templates

--- templates/Admin/QrCodes/details.php
+++ templates/Admin/QrCodes/details.php
@@ -6,54 +6,41 @@
 if (!$this->getRequest()->is('ajax')) {
     $this->setLayout('dashboard/view');
 }
-$action = $this->getRequest()->getParam('action');
 
-$tabs = [];
+$this->assign('page_title', $qrCode->name);
 
 if ($qrCode->id) {
+    $this->start('page_options');
+
+    $action = $this->getRequest()->getParam('action');
     $tabs = [
         'view' => [__('Details'), ['controller' => 'QrCodes', 'action' => 'view', $qrCode->id]],
         'edit' => [__('Edit'), ['controller' => 'QrCodes', 'action' => 'edit', $qrCode->id]],
         'qrCode' => [__('Images'), ['controller' => 'QrImages', 'action' => 'qrCode', $qrCode->id]],
     ];
+
+    ?>
+    <ul class="list-inline">
+    <?php foreach ($tabs as $k => $tab) :
+        $options = [
+            'class' => 'underline text-black',
+        ];
+        if ($k === $action) {
+            $options['class'] .= ' active';
+            $options['aria-current'] = 'page';
+        }
+        ?>
+        <li class="list-inline-item ms-2">
+            <?= $this->Html->link($tab[0], $tab[1], $options) ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php
+    $this->end(); // page_options
 }
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
-<div class="container mt-md-5 px-0 px-md-3">
-    <div class="row align-items-end mb-2">
-        <div class="col">
-            <h2><?= $qrCode->name ?></h2>
-        </div>
-    </div>
-    <div class="row mb-2">
-        <div class="col text-end">
-            <ul class="list-inline">
-            <?php foreach ($tabs as $k => $tab) :
-                $options = [
-                    'class' => 'underline text-black',
-                ];
-                if ($k === $action) {
-                    $options['class'] .= ' active';
-                    $options['aria-current'] = 'page';
-                }
-                ?>
-                <li class="list-inline-item ms-2">
-                    <?= $this->Html->link($tab[0], $tab[1], $options) ?>
-                </li>
-            <?php endforeach; ?>
-            </ul>
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="card bg-opaque-white">
-                <div class="card-body bg-white">
-                    <?= $this->fetch('content'); ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<?= $this->fetch('content'); ?>
 
 <?= $this->Template->templateComment(false, __FILE__); ?>
