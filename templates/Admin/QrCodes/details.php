e3f92796b1a60cba17ba5640e82db1dfca0b0032
Brian French <fr3nch13@gmail.com>
2023-10-11 21:54:44 -0700

    Update details.php

--- templates/Admin/QrCodes/details.php
+++ templates/Admin/QrCodes/details.php
@@ -7,8 +7,22 @@ if (!$this->getRequest()->is('ajax')) {
     $this->setLayout('dashboard/view');
 }
 ?>
-tabs go here.
 <?= $this->Template->templateComment(true, __FILE__); ?>
+
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
 
 <?= $this->fetch('content'); ?>
 
