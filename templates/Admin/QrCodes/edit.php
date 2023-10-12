0a84dfab26e3e711cd82040d1967625e21dc5fd7
Brian French <fr3nch13@gmail.com>
2023-10-12 15:42:27 -0700

    Put forms back in a card.

--- templates/Admin/QrCodes/edit.php
+++ templates/Admin/QrCodes/edit.php
@@ -12,37 +12,41 @@ if (!$this->getRequest()->is('ajax')) {
 }
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $qrCode->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $qrCode->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List QR Codes'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="qrCodes form content">
-            <?= $this->Form->create($qrCode) ?>
-            <fieldset>
-                <legend><?= __('Edit QR Code') ?></legend>
+<div class="card bg-opaque-white">
+    <div class="card-body bg-white p-2 p-lg-5">
+        <div class="row">
+            <aside class="column">
+                <div class="side-nav">
+                    <h4 class="heading"><?= __('Actions') ?></h4>
+                    <?= $this->Form->postLink(
+                        __('Delete'),
+                        ['action' => 'delete', $qrCode->id],
+                        ['confirm' => __('Are you sure you want to delete # {0}?', $qrCode->id), 'class' => 'side-nav-item']
+                    ) ?>
+                    <?= $this->Html->link(__('List QR Codes'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+                </div>
+            </aside>
+            <div class="column column-80">
+                <div class="qrCodes form content">
+                    <?= $this->Form->create($qrCode) ?>
+                    <fieldset>
+                        <legend><?= __('Edit QR Code') ?></legend>
 
-                <?= $this->Form->control('name'); ?>
+                        <?= $this->Form->control('name'); ?>
 
-                <?= $this->Form->control('description'); ?>
+                        <?= $this->Form->control('description'); ?>
 
-                <?= $this->Form->control('url'); ?>
+                        <?= $this->Form->control('url'); ?>
 
-                <?= $this->Form->control('source_id', ['options' => $sources]); ?>
+                        <?= $this->Form->control('source_id', ['options' => $sources]); ?>
 
-                <?= $this->Form->control('tags._ids', ['options' => $tags]); ?>
+                        <?= $this->Form->control('tags._ids', ['options' => $tags]); ?>
 
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+                    </fieldset>
+                    <?= $this->Form->button(__('Submit')) ?>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
