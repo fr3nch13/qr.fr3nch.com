c1d7c096e24fa6da70e38ea70496710d35a7abc9
Brian French <fr3nch13@gmail.com>
2023-10-11 23:47:01 -0700

    Update details.php

--- templates/Admin/QrCodes/details.php
+++ templates/Admin/QrCodes/details.php
@@ -16,7 +16,7 @@ $tabs = [
 ?>
 <?= $this->Template->templateComment(true, __FILE__); ?>
 <div class="container mt-5">
-    <div class="row mb-2">
+    <div class="row align-items-end mb-2">
         <div class="col-md-6">
             <h2><?= $qrCode->name ?></h2>
         </div>
