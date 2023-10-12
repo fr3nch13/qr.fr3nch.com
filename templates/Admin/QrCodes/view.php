655d1e8afceadbc2e7bce3d79fc2388642369efe
Brian French <fr3nch13@gmail.com>
2023-10-12 15:27:42 -0700

    Update view.php

--- templates/Admin/QrCodes/view.php
+++ templates/Admin/QrCodes/view.php
@@ -36,8 +36,8 @@ if (!$this->getRequest()->is('ajax')) {
                     <dt class="col-3"><?= __('Last Hit') ?></dt>
                     <dd class="col-9"><?= h($qrCode->last_hit) ?> </dd>
 
-                    <dt class="col-6 col-md-3"><?= __('Created') ?></dt>
-                    <dd class="col-6 col-md-9"><?= h($qrCode->created) ?> </dd>
+                    <dt class="col-5 col-md-3"><?= __('Created') ?></dt>
+                    <dd class="col-7 col-md-9"><?= h($qrCode->created) ?> </dd>
 
                     <dt class="col-3"><?= __('Source') ?></dt>
                     <dd class="col-9"><?= $qrCode->hasValue('source') ? $this->Html->link(
