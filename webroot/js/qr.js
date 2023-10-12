c34758b1ff14e83ea483806b9883b28400dfb017
Brian French <fr3nch13@gmail.com>
2023-10-12 16:54:51 -0700

    Initial tags input.

--- webroot/js/qr.js
+++ webroot/js/qr.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
 
+    // if a tags input is detected, apply the bootstrap5-tags module.
+    if ($('select.tags-input').length) {
+        import Tags from "../assets/npm-asset/bootstrap5-tags/tags.js";
+        $('select.tags-input').each(function( index, element ){
+            Tags.init(element);
+        });
+    }
 });
