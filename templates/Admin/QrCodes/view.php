c2760a87c7b15b89483d7a2ef871e5e9f200316b
Brian French <fr3nch13@gmail.com>
2023-10-12 19:21:11 -0700

    Update view.php

--- templates/Admin/QrCodes/view.php
+++ templates/Admin/QrCodes/view.php
@@ -67,7 +67,7 @@ if (!$this->getRequest()->is('ajax')) {
                     <p class="text-secondary mb-3"><?= $this->Text->autoParagraph(h($qrCode->description)) ?></p>
                 </div>
             </div>
-            <div class="col order-2 order-lg-3 mb-2 border-top">
+            <div class="col order-2 order-lg-3 my-2 border-top">
                 <h5><?= __('Tags') ?></h5>
                 <?php foreach ($qrCode->tags as $tag) : ?>
                     <span class="my-1 my-md-2 mx-1 mx-md-2 btn btn-sm btn-light btn-outline-secondary rounded-pill" role="button"><?= $tag->name ?></span>
