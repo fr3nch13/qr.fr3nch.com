d05499cf2c02956cebf59e125a35d19f5ee3fbf6
Brian French <fr3nch13@gmail.com>
2023-10-12 17:01:29 -0700

    Update qr.js

--- webroot/js/qr.js
+++ webroot/js/qr.js
@@ -1,8 +1,5 @@
 
 import Tags from "../assets/npm-asset/bootstrap5-tags/tags.js";
 $( document ).ready(function() {
-    // if a tags input is detected, apply the bootstrap5-tags module.
-    if ($('select.tags-input').length) {
-        Tags.init('select.tags-input');
-    }
+    Tags.init('select.tags-input');
 });
