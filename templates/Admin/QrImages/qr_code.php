c1a8e6d285a9307846df621fcf5858fb6e355969
Brian French <fr3nch13@gmail.com>
2023-10-14 20:43:29 -0700

    Update qr_code.php

--- templates/Admin/QrImages/qr_code.php
+++ templates/Admin/QrImages/qr_code.php
@@ -33,14 +33,19 @@ if (!$this->getRequest()->is('ajax')) {
                 <?php endif; ?>
                 <div class="card-body p-3">
                     <div class="card-title"><?= $qrImage->name ?></div>
-                    <img
-                        class="img-fluid"
-                        src="<?= $this->Url->build([
-                            'action' => 'show',
-                            $qrImage->id,
-                            '?' => ['thumb' => 'sm'],
-                            ]) ?>"
-                        alt="<?= $qrImage->name ?>">
+                    <a
+                        href="#"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modal-<?= $qrImage->id ?>">
+                        <img
+                            class="img-fluid"
+                            src="<?= $this->Url->build([
+                                'action' => 'show',
+                                $qrImage->id,
+                                '?' => ['thumb' => 'sm'],
+                                ]) ?>"
+                            alt="<?= $qrImage->name ?>">
+                    </a>
                 </div>
                 <div class="card-footer text-muted p-0 btn-group">
                     <?= $this->Html->link(__('Edit'), [
@@ -51,6 +56,13 @@ if (!$this->getRequest()->is('ajax')) {
                         'data-bs-toggle' => 'modal',
                         'data-bs-target' => '#edit-modal',
                         'data-ajax-target' => '#editModalBody',
+                    ]) ?>
+                    <?= $this->Html->link(__('Download'), [
+                        'action' => 'show',
+                        $qrImage->id,
+                        '?' => ['download' => true]
+                    ], [
+                        'class' => 'btn btn-sm btn-light',
                     ]) ?>
 
                     <?= $this->Form->postLink(__('Delete'), [
@@ -70,18 +82,57 @@ if (!$this->getRequest()->is('ajax')) {
 </div>
 
 <?php $this->start('modal') ?>
+
+<?php foreach ($qrImages as $qrImage) : ?>
+<!-- Image Viewer -->
+<div
+    class="modal fade"
+    id="modal-<?= $qrImage->id ?>"
+    tabindex="-1"
+    aria-labelledby="modalLabel-<?= $qrImage->id ?>"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <button
+                type="button"
+                class="bi bi-x modal-close text-white"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            <div class="modal-body m-0 p-0">
+                <div class="row m-0 p-0">
+                    <div class="col m-0 p-0">
+                        <img
+                            class="img-fluid"
+                            src="<?= $this->Url->build([
+                                'action' => 'show',
+                                $qrImage->id,
+                                '?' => ['thumb' => 'lg'],
+                                ]) ?>"
+                            alt="<?= $qrImage->name ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endforeach; ?>
 <!-- Edit Modal -->
 
 <div class="modal fade" id="edit-modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <button type="button" class="bi bi-x modal-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        <div class="modal-body p-4">
-          <span id="editModalLabel" class="invisible"><?= __('Edit') ?></span>
-          <div id="editModalBody"></div>
+        <div class="modal-content">
+            <button
+                type="button"
+                class="bi bi-x modal-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            <div class="modal-body p-4">
+                <span id="editModalLabel" class="invisible"><?= __('Edit') ?></span>
+                <div id="editModalBody"></div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+
 <?php $this->end(); // modal ?>
   <?= $this->Template->templateComment(false, __FILE__); ?>
