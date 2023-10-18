57f112451ba347bb51921f625eed7d120e8c0a08
Brian French <fr3nch13@gmail.com>
2023-10-18 13:44:07 -0700

    Fix html

--- templates/Admin/Users/index.php
+++ templates/Admin/Users/index.php
@@ -41,27 +41,26 @@ $this->assign('page_title', __('Users'));
                         </div>
 
                         <div class="card-body p-2 border-top">
-                            <div class="row fs-80">
-                                    <dt class="col-12 col-md-4"><?= __('Email') ?></dt>
-                                    <dd class="col-12 col-md-8"><?= h($user->email) ?> </dd>
+                            <dl class="row fs-80">
+                                <dt class="col-12 col-md-4"><?= __('Email') ?></dt>
+                                <dd class="col-12 col-md-8"><?= h($user->email) ?> </dd>
 
-                                    <dt class="col-4"><?= __('Admin') ?></dt>
-                                    <dd class="col-8"><?php
-                                    if ($user->is_admin) {
-                                        echo $this->Html->icon('check');
-                                    } else {
-                                        echo $this->Html->icon('x');
-                                    }
-                                    ?></dd>
+                                <dt class="col-4"><?= __('Admin') ?></dt>
+                                <dd class="col-8"><?php
+                                if ($user->is_admin) {
+                                    echo $this->Html->icon('check');
+                                } else {
+                                    echo $this->Html->icon('x');
+                                }
+                                ?></dd>
 
-                                    <dt class="col-4"><?= __('Created') ?></dt>
-                                    <dd class="col-8"><?php
-                                    if ($user->created) {
-                                        echo $user->created->format('M d, Y');
-                                    }
-                                    ?></dd>
-                                </dl>
-                            </div>
+                                <dt class="col-4"><?= __('Created') ?></dt>
+                                <dd class="col-8"><?php
+                                if ($user->created) {
+                                    echo $user->created->format('M d, Y');
+                                }
+                                ?></dd>
+                            </dl>
                         </div>
                     </div>
 
@@ -87,59 +86,6 @@ $this->assign('page_title', __('Users'));
         </div>
     </div>
 </div>
-<!--
-<div class="users index content">
-    <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Users') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('email') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($users as $user) : ?>
-                <tr>
-                    <td><?= $this->Number->format($user->id) ?></td>
-                    <td><?= h($user->name) ?></td>
-                    <td><?= h($user->email) ?></td>
-                    <td><?= h($user->created) ?></td>
-                    <td><?= h($user->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $user->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), [
-                            'action' => 'delete',
-                            $user->id,
-                        ], [
-                            'confirm' => __('Are you sure you want to delete # {0}?', $user->id),
-                        ]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, ' .
-            'showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
-</div>
--->
-
 
 <?php $this->start('page_options'); ?>
 <ul class="list-inline">
